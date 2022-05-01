35296660810e6dc148fd19a474bc7733872ed064
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-05-01 23:19:21 +0900

    feat(Product)

--- gc-coffee/src/main/java/com/example/gccoffee/model/Product.java
+++ gc-coffee/src/main/java/com/example/gccoffee/model/Product.java
@@ -14,6 +14,16 @@ public class Product {
     private final LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
+    public Product(UUID productId, String productName, Category category, long price, String description, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
     public Product(UUID productId, String productName, Category category, long price) {
         this.productId = productId;
         this.productName = productName;
