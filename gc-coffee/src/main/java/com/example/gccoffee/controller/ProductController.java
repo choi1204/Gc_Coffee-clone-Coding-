3a3206f11245da0a2ebd40f4a15d71e7db5fd0be
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-05-02 17:58:14 +0900

    feat(ProductController). template(ProductList)

---
+++ gc-coffee/src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.example.gccoffee.controller;
+
+import com.example.gccoffee.model.Product;
+import com.example.gccoffee.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String productPage(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+}
