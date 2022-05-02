f5da3310af4478b04a97c4f2d15845aa05be18db
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-05-02 16:34:08 +0900

    feat(ProductRepositoryImpl)

--- gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepositoryImpl.java
+++ gc-coffee/src/main/java/com/example/gccoffee/repository/ProductJdbcRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.example.gccoffee.repository;
 
 import com.example.gccoffee.model.Category;
 import com.example.gccoffee.model.Product;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -27,8 +28,9 @@ public class ProductJdbcRepositoryImpl implements ProductRepository {
 
     @Override
     public Product insert(Product product) {
-        var update =  jdbcTemplate.update("INSERT INTO products(product_id, product_name, category, price, description, created_at, updated_at)" +
-                " VALUES( UUID_TO_BIN(:productId), :productName, :category, :price, :description, :createdAt, :updatedAt)", toParamMap(product));
+        var update = jdbcTemplate.update("INSERT INTO products(product_id, product_name, category, price, description, created_at, updated_at)" +
+                " VALUES(UNHEX(REPLACE(:productId, '-', '')), :productName, :category, :price, :description, :createdAt, :updatedAt)", toParamMap(product));
+
         if (update != 1)
             throw new RuntimeException("Notion was interested");
         return product;
@@ -36,45 +38,70 @@ public class ProductJdbcRepositoryImpl implements ProductRepository {
 
     @Override
     public Product update(Product product) {
-        return null;
+        int update = jdbcTemplate.update(
+                "UPDATE products SET product_name = :productName, category = :category, price = :price, description = :description, created_at = :createdAt, updated_at = :updatedAt " +
+                        "WHERE product_id = UNHEX(REPLACE(:productId, '-', ''))", toParamMap(product)
+        );
+        if (update != 1) {
+            throw new RuntimeException("Nothing was update");
+        }
+        return product;
     }
 
     @Override
     public Optional<Product> findById(UUID productId) {
-        return Optional.empty();
+
+        try {
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_id = UNHEX(REPLACE(:productId, '-', ''))",
+                            Collections.singletonMap("productId", productId.toString().getBytes()), productRowMapper)
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public Optional<Product> findByName(String productName) {
-        return Optional.empty();
+        try {
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject("SELECT * FROM products WHERE product_name = :productName",
+                            Collections.singletonMap("productName", productName), productRowMapper)
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Product> findByCategory(Category category) {
-        return null;
+        return jdbcTemplate.query(
+                "SELECT * FROM products WHERE category = :category",
+                Collections.singletonMap("category", category.toString()), productRowMapper
+        );
     }
 
     @Override
     public void deleteAll() {
-
+        jdbcTemplate.update("DELETE FROM products", Collections.emptyMap());
     }
 
     private static final RowMapper<Product> productRowMapper = (rs, rowNum) -> {
         var productId = toUUID(rs.getBytes("product_id"));
         var productName = rs.getString("product_name");
-        var category= Category.valueOf(rs.getString("category"));
-        var price= rs.getLong("price");
-        var description= rs.getString("description");
-        var createdAt= toLocalDateTime(rs.getTimestamp("createdAt"));
-        var updatedAt= toLocalDateTime(rs.getTimestamp("updatedAt"));
+        var category = Category.valueOf(rs.getString("category"));
+        var price = rs.getLong("price");
+        var description = rs.getString("description");
+        var createdAt = toLocalDateTime(rs.getTimestamp("created_at"));
+        var updatedAt = toLocalDateTime(rs.getTimestamp("updated_at"));
         return new Product(productId, productName, category, price, description, createdAt, updatedAt);
     };
 
     private Map<String, Object> toParamMap(Product product) {
         var paramMap = new HashMap<String, Object>();
-        paramMap.put("productId", product.getProductId());
+        paramMap.put("productId", product.getProductId().toString());
         paramMap.put("productName", product.getProductName());
-        paramMap.put("category", product.getCategory());
+        paramMap.put("category", product.getCategory().toString());
         paramMap.put("price", product.getPrice());
         paramMap.put("description", product.getDescription());
         paramMap.put("createdAt", product.getCreatedAt());
