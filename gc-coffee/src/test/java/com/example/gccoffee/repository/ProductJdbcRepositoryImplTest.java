6b8f07fbf30898e3472e60ef3da076c1ad682535
choi1204 <76509935+choi1204@users.noreply.github.com>
2022-05-02 16:34:40 +0900

    test(ProductRepositroy) : wix 의존성 및 Product Repostitory 테스트

---
+++ gc-coffee/src/test/java/com/example/gccoffee/repository/ProductJdbcRepositoryImplTest.java
@@ -0,0 +1,115 @@
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import com.wix.mysql.EmbeddedMysql;
+import com.wix.mysql.ScriptResolver;
+import com.wix.mysql.config.Charset;
+import com.wix.mysql.config.MysqldConfig;
+import com.wix.mysql.distribution.Version;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig;
+import static com.wix.mysql.distribution.Version.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+class ProductJdbcRepositoryImplTest {
+
+    static EmbeddedMysql embeddedMysql;
+
+    @BeforeAll
+    static void setup() {
+        MysqldConfig config = aMysqldConfig(v5_7_latest)
+                .withCharset(Charset.UTF8)
+                .withPort(2215)
+                .withUser("test", "test1234!")
+                .withTimeZone("Asia/Seoul")
+                .build();
+        embeddedMysql = EmbeddedMysql.anEmbeddedMysql(config)
+                .addSchema("test-order_mgmt", ScriptResolver.classPathScript("schema.sql"))
+                .start();
+    }
+
+    @AfterAll
+    static void cleanUp() {
+        embeddedMysql.stop();
+    }
+
+    @Autowired
+    ProductRepository repository;
+
+    private static Product newProduct = new Product(UUID.randomUUID(), "new-product", Category.COFFEE_BEAN_PACKAGE, 1000L);
+
+    @Test
+    @Order(1)
+    @DisplayName("상품을 추가할 수 있다.")
+    void testInsert() {
+        repository.insert(newProduct);
+        List<Product> all = repository.findAll();
+        assertThat(all.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("상품을 이름으로 조회할 수 있다.")
+    void testFindByName() {
+        var product = repository.findByName(newProduct.getProductName());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("상품을 상품아이디로 조회할 수 있다.")
+    void testFindById() {
+        var product = repository.findById(newProduct.getProductId());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("상품을 상품 카테고리으로 조회할 수 있다.")
+    void testFindByCategory() {
+        var product = repository.findByCategory(newProduct.getCategory());
+        assertThat(product.isEmpty(), is(false));
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("상품을 수정할 수 있다.")
+    void testUpdate() {
+        newProduct.setProductName("updated-Product");
+        repository.update(newProduct);
+
+        Optional<Product> product = repository.findById(newProduct.getProductId());
+        assertThat(product.isEmpty(), is(false));
+        assertThat(product.get(), samePropertyValuesAs(newProduct));
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("상품을 전체 삭제한다.")
+    void testDeleteAll() {
+        repository.deleteAll();
+        List<Product> all = repository.findAll();
+        assertThat(all.isEmpty(), is(true));
+    }
+
+}
